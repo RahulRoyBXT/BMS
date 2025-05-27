3cc3a32584625fc560fe592bd8c7ab0e940c9000
Rahul Roy <rahulroy.it2021@nsec.ac.in>
2025-05-27 18:56:59 +0530

    feat: Add user authentication forms and validation schemas - Implemented UpdateForm component for user updates. - Enhanced login and signup actions with detailed error handling. - Introduced login and register schemas for form validation.

--- src/common/schemas/userSchema.ts
+++ src/common/schemas/userSchema.ts
@@ -6,3 +6,27 @@ export const userUpdateSchema = z.object({
 })
 
 export type UserUpdateType = z.infer<typeof userUpdateSchema>
+
+// Register Schema
+
+export const loginSchema = z.object({
+    email: z.string()
+        .min(8, 'At least 8 Character required')
+        .regex(/[a-z]/, 'Email must contain at least one lowercase letter'),
+    password: z.string()
+        .regex(/[A-Z]/, 'Password must contain at least one uppercase letter')
+        .regex(/[a-z]/, 'Password must contain at least one lowercase letter')
+        .regex(/[0-9]/, 'Password must contain at least one number')
+        .regex(/[^A-Za-z0-9]/, 'Password must contain at least one special character')
+})
+
+export type LoginType = z.infer<typeof loginSchema>
+
+export const registerSchema = loginSchema.extend({
+    confirmPassword : z.string()
+}).refine((data)=> data.password === data.confirmPassword, {
+    message: 'Password Does not match',
+    path:["confirmPassword"]
+})
+
+export type RegisterType = z.infer<typeof registerSchema>
